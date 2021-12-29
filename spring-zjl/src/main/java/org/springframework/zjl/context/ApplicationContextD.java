0b8c4311d327ad3bd5310d8648c888c965c9fde5
Gary <cnahzjl@gmail.com>
2021-12-29 17:56:28 +0800

    添加测试模块

---
+++ spring-zjl/src/main/java/org/springframework/zjl/context/ApplicationContextD.java
@@ -0,0 +1,15 @@
+package org.springframework.zjl.context;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * @author zJiaLi
+ * @since 2021-12-29 15:09
+ */
+public class ApplicationContextD {
+
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(MainConfig.class);
+		applicationContext.refresh();
+	}
+}
---
+++ spring-zjl/src/main/java/org/springframework/zjl/context/MainConfig.java
@@ -0,0 +1,20 @@
+package org.springframework.zjl.context;
+
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author zJiaLi
+ * @since 2021-12-29 15:10
+ */
+@ComponentScan(basePackages = {"org.springframework.zjl.context"})
+@Configuration
+public class MainConfig implements BeanNameAware {
+
+	@Override
+	public void setBeanName(String name) {
+		System.out.println("MainConfig bean Name: " + name);
+	}
+}
